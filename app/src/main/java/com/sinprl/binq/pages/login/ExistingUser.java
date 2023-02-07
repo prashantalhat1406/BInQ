a9f20414792a8a2a172224d5660e47a9dfa830f3
Prashant Alhat <prashant.alhat@gmail.com>
2023-02-07 11:10:38 +0530

    login activities added

---
+++ app/src/main/java/com/sinprl/binq/pages/login/ExistingUser.java
@@ -0,0 +1,16 @@
+package com.sinprl.binq.pages.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sinprl.binq.R;
+
+public class ExistingUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_user);
+    }
+}
--- app/src/main/java/com/sinprl/binq/pages/login/Front.java
+++ app/src/main/java/com/sinprl/binq/pages/login/Front.java
@@ -2,10 +2,14 @@ package com.sinprl.binq.pages.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.sinprl.binq.R;
+import com.sinprl.binq.pages.appointment_admin.Appointment_Add;
+import com.sinprl.binq.pages.appointment_admin.Appointment_Display;
 
 public class Front extends AppCompatActivity {
 
@@ -17,6 +21,20 @@ public class Front extends AppCompatActivity {
         setContentView(R.layout.activity_front);
 
         but_newuser = findViewById(R.id.but_front_new_user);
+        but_newuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Front.this, NewUser.class);
+                startActivity(intent);
+            }
+        });
         but_existing_user = findViewById(R.id.but_front_existing_user);
+        but_existing_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Front.this, ExistingUser.class);
+                startActivity(intent);
+            }
+        });
     }
 }
