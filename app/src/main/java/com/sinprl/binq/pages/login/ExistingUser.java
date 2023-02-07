04e7c2715b589b3a3198d25163bb22e108b1da35
Prashant Alhat <prashant.alhat@gmail.com>
2023-02-07 11:38:22 +0530

    new user added to database

--- app/src/main/java/com/sinprl/binq/pages/login/ExistingUser.java
+++ app/src/main/java/com/sinprl/binq/pages/login/ExistingUser.java
@@ -3,14 +3,26 @@ package com.sinprl.binq.pages.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.sinprl.binq.R;
 
 public class ExistingUser extends AppCompatActivity {
 
+    Button but_login, but_cancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_existing_user);
+
+        but_cancel = findViewById(R.id.but_existing_user_cancel);
+        but_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/sinprl/binq/pages/login/NewUser.java
+++ app/src/main/java/com/sinprl/binq/pages/login/NewUser.java
@@ -3,14 +3,54 @@ package com.sinprl.binq.pages.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sinprl.binq.R;
+import com.sinprl.binq.dataclasses.Appointment;
+import com.sinprl.binq.dataclasses.User;
+import com.sinprl.binq.utils.Utils;
 
 public class NewUser extends AppCompatActivity {
+
+    Button but_register, but_cancel;
+    FirebaseDatabase database;
+    EditText edt_user_name,edt_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        database = FirebaseDatabase.getInstance("https://binq-1171a-default-rtdb.asia-southeast1.firebasedatabase.app");
+
+        but_register = findViewById(R.id.but_new_user_register);
+        but_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add_user_details_to_database();
+            }
+        });
+        but_cancel = findViewById(R.id.but_new_user_cancel);
+        but_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void add_user_details_to_database() {
+        edt_user_name = findViewById(R.id.edt_new_user_name);
+        edt_phone = findViewById(R.id.edt_new_user_phone);
+
+        User user = new User(edt_user_name.getText().toString(),
+                edt_phone.getText().toString());
+
+        DatabaseReference databaseReference = database.getReference("Users/");
+        databaseReference.child(edt_phone.getText().toString()).setValue(user);
     }
 }
