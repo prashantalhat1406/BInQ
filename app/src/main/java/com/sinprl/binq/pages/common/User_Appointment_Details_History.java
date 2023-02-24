66335a6b730994ec87e742a7a09e1c8f1247f413
Prashant Alhat <prashant.alhat@gmail.com>
2023-02-24 12:40:35 +0530

    appointment details list added

---
+++ app/src/main/java/com/sinprl/binq/pages/common/User_Appointment_Details_History.java
@@ -0,0 +1,16 @@
+package com.sinprl.binq.pages.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sinprl.binq.R;
+
+public class User_Appointment_Details_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_appointment_details_history);
+    }
+}
